fc3e597b8ff841bce6980f12e6140266328e8ce6
AmosKimeli <kimeliamose7830@gmail.com>
2022-12-09 01:18:04 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,26 +1,14 @@
 #include <stdio.h>
 
-
 /**
  * main - Etry Point
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
-
-}
-
-
-
-
-
- 
-
-
-
-
 
 }	
